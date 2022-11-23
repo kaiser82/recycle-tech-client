5e67790e1135ee4bb5600e8deb90059ade68bf8c
kaiser82 <dr.kaiserahmed@gmail.com>
2022-11-24 01:55:34 +0600

    sign in and up form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>SignUp page</h2>
-        </div>
+        <section class="bg-gray-100">
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <Link to='/' class="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
+                    <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    RecycleTech
+                </Link>
+                <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
+                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            Create an account
+                        </h1>
+                        <form class="space-y-4 md:space-y-6">
+                            <div>
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Your name</label>
+                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Ariana Grande" required="" />
+                            </div>
+                            <div>
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
+                            </div>
+                            <div>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                            </div>
+                            <div>
+                                <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
+                                <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+                            </div>
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="" />
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="terms" class="font-light text-gray-500 ">I accept the <Link class="font-medium text-primary-600 hover:underline " to='#'>Terms and Conditions</Link></label>
+                                </div>
+                            </div>
+                            <button type="submit" class="w-full text-white btn bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
+                            <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-3 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1  focus:ring-violet-400">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                                    <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                                </svg>
+                                <p>Login with Google</p>
+                            </button>
+                            <p class="text-sm font-light text-gray-500 ">
+                                Already have an account? <Link to='/login' class="font-medium text-primary-600 hover:underline ">Login here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,6 +3,8 @@ import Dashboard from "../../Layout/Dashboard";
 import Main from "../../Layout/Main";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -13,7 +15,15 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
-            }
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
         ]
     },
     {
