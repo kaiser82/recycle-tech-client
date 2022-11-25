fa6128e8ba71aaaf9b73ccb0380117f4feaa95bb
kaiser82 <dr.kaiserahmed@gmail.com>
2022-11-26 05:22:08 +0600

    add product

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,58 +1,55 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider';
 
 const AddProduct = () => {
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     const { user } = useContext(AuthContext);
-    // const [createdUserEmail, setCreatedUserEmail] = useState('');
-    // const [token] = useToken(createdUserEmail);
-    // const navigate = useNavigate();
 
-    // if (token) {
-    //     navigate('/');
-    // }
+    const navigate = useNavigate();
 
     const handleAddProduct = (data) => {
-        console.log(data);
-        // createUser(data.email, data.password)
-        //     .then(res => {
 
-        //         const user = res.user;
-        //         console.log(user);
-        //         toast.success("New user created successfully.")
-        //         const userInfo = {
-        //             displayName: data.name
-        //         }
-        //         updateUser(userInfo)
-        //             .then(() => {
-        //                 saveUser(data.name, data.email, data.role);
-        //             })
-        //             .catch(e => console.log(e.message))
-        //     })
-        //     .catch(error => {
-        //         toast.error(error.message);
+        const today = new Date();
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const postTime = date + ' ' + time;
 
-        //     });
+        const { category_id, productName, image, price, resalePrice, yearsOfUse, location, mobile } = data
+
+        const product = {
+            category_id,
+            productName,
+            image,
+            price,
+            resalePrice,
+            yearsOfUse,
+            location,
+            mobile,
+            seller: user.displayName,
+            email: user.email,
+            time: postTime
+        }
+        console.log(product)
+
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast.success('Product added successfully.');
+                navigate('/dashboard/myproducts')
+            })
     }
 
-
-    // const saveUser = (name, email, role) => {
-    //     const user = { name, email, role };
-    //     fetch('http://localhost:5000/users', {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(user)
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log('save user:', data);
-    //             // setCreatedUserEmail(email);
-    //         })
-    // }
 
     return (
         <section className="bg-gray-100">
@@ -65,13 +62,15 @@ const AddProduct = () => {
                         <form onSubmit={handleSubmit(handleAddProduct)} className="space-y-4 md:space-y-6">
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Product name</label>
-                                <input type="text" name="name" {...register("name", { required: "Name field cannot be empty!" })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Lenovo Legion" />
-                                {errors.name && <span className='label-text text-red-500'>{errors.name?.message}</span>}
+                                <input type="text" name="name" {...register("productName", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Lenovo Legion" />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Price</label>
-                                <input type="number" name="email" {...register("price", { required: "Email field cannot be empty!" })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="12345" />
-                                {errors.email && <span className='label-text text-red-500'>{errors.email?.message}</span>}
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Resale Price</label>
+                                <input type="number" name="resalePrice" {...register("resalePrice", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="12345" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Original Price</label>
+                                <input type="number" name="email" {...register("price", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="12345" />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Product condition</label>
@@ -83,31 +82,33 @@ const AddProduct = () => {
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Mobile no</label>
-                                <input type="number" name="email" {...register("mobile", { required: "Email field cannot be empty!" })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="01711123123" />
-                                {errors.email && <span className='label-text text-red-500'>{errors.email?.message}</span>}
+                                <input type="number" name="email" {...register("mobile", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="01711123123" />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Location</label>
-                                <input type="text" name="name" {...register("location", { required: "Name field cannot be empty!" })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Dhaka" />
+                                <input type="text" name="location" {...register("location", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Dhaka" />
                                 {errors.name && <span className='label-text text-red-500'>{errors.name?.message}</span>}
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Product category</label>
-                                <select {...register("condition")} className="select select-bordered w-full rounded-lg">
-                                    <option >excellent</option>
-                                    <option>good</option>
-                                    <option>fair</option>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Product category ID</label>
+                                <select {...register("category_id")} className="select select-bordered w-full rounded-lg">
+                                    <option >637f2a88460053f520faaa1d</option>
+                                    <option>637f2a88460053f520faaa1e</option>
+                                    <option>637f2a88460053f520faaa1f</option>
                                 </select>
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Year used</label>
-                                <input type="number" name="password"
-                                    {...register("password", { required: "Password field cannot be empty!" })}
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Years of used</label>
+                                <input type="double" name="used"
+                                    {...register("yearsOfUse", { required: true })}
                                     placeholder="12345" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " />
-                                {errors.password && <span className='label-text text-red-500'>{errors.password?.message}</span>}
                             </div>
-
-
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Image URL</label>
+                                <input type="text" name="used"
+                                    {...register("image", { required: true })}
+                                    placeholder="https://www.google.com/" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " />
+                            </div>
                             <button type="submit" className="w-full text-white btn bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Add Product</button>
 
                         </form>
